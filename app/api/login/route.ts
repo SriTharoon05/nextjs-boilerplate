cae69efe747fa3f08dcc6b1fb80876c2322bc421
SriTharoon A.S <sritharoon05@gmail.com>
2025-12-02 18:26:20 +0530

    username updated

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,7 +3,7 @@
 export async function POST(request: Request) {
   const body = await request.text();
 
-  const response = await fetch('https://portal.ubtiinc.com/TimetrackForms/Account/Login', {
+  const response = await fetch('https://portal.ubtiinc.com/TimetrackForms/Login/UsernamePassword', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
