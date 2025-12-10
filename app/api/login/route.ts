ea32f147625cc3b48d159cf137fcf9fe722c5774
SriTharoon A.S <sritharoon05@gmail.com>
2025-12-10 17:45:19 +0530

    Update CORS policy to allow requests from the Sigma Unlimited admin site

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': 'http://localhost:5173',
+          'Access-Control-Allow-Origin': 'http://localhost:5173, https://sigmaunlimited.netlify.app/admin',
           'Access-Control-Allow-Credentials': 'true',
         },
       }
